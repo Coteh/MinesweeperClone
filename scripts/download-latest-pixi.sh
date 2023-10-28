e58e2ff9ab215c5b776bf74d5ca8c1e6e15bd9a0
Coteh <3276350+Coteh@users.noreply.github.com>
2023-10-27 21:08:46 -0400

    Remove CLI, update build/deploy scripts, replace Travis with GitHub Actions
    Other Changes
    - Add dependabot and kodiak config files
    - Add GitHub issue templates

--- scripts/download-latest-pixi.sh
+++ scripts/download-latest-pixi.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 mkdir pixi
 cd pixi
-curl --remote-name-all http://pixijs.download/release/pixi{.js,.js.map}
+curl --remote-name-all https://raw.githubusercontent.com/pixijs/pixijs/v4.3.0/dist/pixi{.js,.js.map}
 if [ $# -gt 0 ] && { [ "$1" = "separate" ] || [ "$1" = "s" ]; }; then
-    curl --remote-name-all https://raw.githubusercontent.com/pixijs/pixi-filters/publish/bin/pixelate{.js,.js.map}
+    curl --remote-name-all https://raw.githubusercontent.com/pixijs/filters/v1.0.6/bin/pixelate{.js,.js.map}
 else
-    curl --remote-name-all https://raw.githubusercontent.com/pixijs/pixi-filters/publish/bin/filters{.js,.js.map}
+    curl --remote-name-all https://raw.githubusercontent.com/pixijs/filters/v1.0.6/bin/filters{.js,.js.map}
 fi
 cd ..
