4d3bece9ec8cfc91a00397a77763c99ef14aefac
James Cote <3276350+Coteh@users.noreply.github.com>
2026-02-08 18:15:45 -0500

    Update readme screenshot

--- cypress/e2e/misc/screenshot.cy.ts
+++ cypress/e2e/misc/screenshot.cy.ts
@@ -1,14 +1,68 @@
 /// <reference types="cypress" />
 
-import { GamePersistentState } from '../../../src/game';
+import { GamePersistentState, GameState } from '../../../src/game';
 import { Preferences } from '../../../src/preferences';
 
 describe('misc', () => {
     beforeEach(() => {
         cy.clearBrowserCache();
         cy.visit('/', {
-            // TODO: Use a pre-generated game board by setting local storage game state
             onBeforeLoad: () => {
+                const boardLayout = [
+                    '111.......',
+                    '2F2.......',
+                    '3F3.111.11',
+                    '2F2.1F222F',
+                    '111.1UUUU2',
+                    '....1U221F',
+                    '11..1F1.11',
+                    'F1..111.1F',
+                    '11......11',
+                    '..........',
+                ];
+                const boardWidth = boardLayout[0].length;
+                const boardHeight = boardLayout.length;
+                const flagCount = boardLayout.reduce(
+                    (acc, row) => acc + row.split('').filter((cell) => cell === 'F').length,
+                    0,
+                );
+                const board = boardLayout.map((row, y) =>
+                    row.split('').map((cell, x) => {
+                        const isFlagged = cell === 'F';
+                        const isMine = isFlagged;
+                        const isUnrevealed = cell === 'U' || isFlagged;
+                        const isRevealed = !isUnrevealed;
+                        const adjMinesCount = cell >= '0' && cell <= '8' ? Number(cell) : 0;
+                        return {
+                            x,
+                            y,
+                            isMine,
+                            isRevealed,
+                            isLosingSpot: false,
+                            isFlagged,
+                            isQuestionMark: false,
+                            adjMinesCount,
+                        };
+                    }),
+                );
+                const gameState: GameState = {
+                    board,
+                    ended: false,
+                    won: false,
+                    firstBlockClicked: true,
+                    score: 0,
+                    didUndo: false,
+                    achievedHighscore: false,
+                    gameOptions: {
+                        boardHeight,
+                        boardWidth,
+                        numberOfMines: flagCount + 10,
+                        revealBoardOnLoss: true,
+                        difficultyKey: 'easy',
+                    },
+                    elapsedTimeMS: 56000,
+                    spareMineSpot: { x: 9, y: 9 },
+                };
                 const persistentState: GamePersistentState = {
                     highscore: {},
                     unlockables: {},
@@ -18,10 +72,12 @@ describe('misc', () => {
                     debugHudEnabled: true,
                     debugHudVisible: false,
                 };
+                window.localStorage.setItem('game-state', JSON.stringify(gameState));
                 window.localStorage.setItem('persistent-state', JSON.stringify(persistentState));
                 window.localStorage.setItem('preferences', JSON.stringify(preferences));
             },
         });
+        cy.waitForGameReady();
     });
 
     specify('gameplay screenshot', () => {
@@ -34,11 +90,6 @@ describe('misc', () => {
 
         // After this delay, the video screenshot should start.
         cy.wait(1000);
-
-        // Hide debug elements from the screenshot
-        cy.get('.debug-link').then((elem) => {
-            elem.css('display', 'none');
-        });
 
         // Static screenshot taken for now
         cy.screenshot('readme/screenshot', {
