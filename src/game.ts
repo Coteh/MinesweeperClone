Coteh/MinesweeperClone

commit 6e1c1ee905ef1c01c13c50b574f49c020b877034

Prevent duplicate spots from being enqueued when visiting neighbour spots to shave off an extra ~0.08 seconds from the tests

src/game.ts
@@ -412,24 +412,24 @@ const performSpotReveal = function (x: number, y: number, callback?: SpotRevealC
 
 const revealSpot: (x: number, y: number) => boolean = function (x: number, y: number) {
     const queue = [{x, y}];
-    const visited = new Set();
+    const visited = new Set([y * gameState.gameOptions.boardWidth + x]);
     let head = 0;
 
     while (head < queue.length) {
         const coords = queue[head++];
-        const coordsKey = coords.y * gameState.gameOptions.boardWidth + coords.x;
-        if (visited.has(coordsKey)) {
-            continue;
-        }
 
         const {isMine, amountOfAdjMines, adjacentSpots} = performSpotReveal(coords.x, coords.y);
-
-        visited.add(coordsKey);
 
         if (!isMine) {
             if (amountOfAdjMines <= 0) {
                 const toAdd = adjacentSpots?.filter(spot => !spot.isFlagged) ?? [];
-                queue.push(...toAdd.map(spot => ({x: spot.x, y: spot.y})));
+                for (const spot of toAdd) {
+                    const key = spot.y * gameState.gameOptions.boardWidth + spot.x;
+                    if (!visited.has(key)) {
+                        visited.add(key);
+                        queue.push({x: spot.x, y: spot.y});
+                    }
+                }
             }
         } else {
             for (let a = 0; a < gameState.gameOptions.boardWidth; a++) {

commit fc3415e07ba24ff7704306341d3030e014e872f7

Remove unneeded spot reveal callback and fix regression with reveal sound effect playing when no new spots revealed
The regression was originally fixed in affdc86. When revealing adjacently, if all the spots were already revealed, the reveal sound should not play. The revealSpot optimization in 6e1c1ee regressed this fix because the initial spot is now always in the visited set which essentially makes it considered newly revealed every time, even if it was already revealed previously.

To address, before calling revealSpot, I check if the spot was already revealed, and if so, I skip the reveal. This fixes the issue once again.

src/game.ts
@@ -82,11 +82,11 @@ export type GameEvent =
 
 export type EventHandler = (event: GameEvent) => void;
 
-type SpotRevealCallback = (
+type SpotRevealResult = {
     isMine: boolean,
     amountOfAdjMines: number,
     adjacentSpots: MineBlock[] | null,
-) => void;
+};
 
 let gameState: GameState = {} as GameState;
 let persistentState: GamePersistentState = {} as GamePersistentState;
@@ -387,7 +387,7 @@ export const selectAdjacentSpots = function (x: number, y: number) {
     return { hitInfo: 'land', win: won };
 };
 
-const performSpotReveal = function (x: number, y: number, callback?: SpotRevealCallback) {
+const performSpotReveal: (x: number, y: number) => SpotRevealResult = function (x, y) {
     gameState.board[y][x].isRevealed = true;
     // Clear any question mark when revealing
     gameState.board[y][x].isQuestionMark = false;
@@ -400,9 +400,6 @@ const performSpotReveal = function (x: number, y: number, callback?: SpotRevealC
         amountOfAdjMines = calculateAdjacentMines(adjacentSpots);
         gameState.board[y][x].adjMinesCount = amountOfAdjMines;
     }
-    if (callback) {
-        callback(isMine, amountOfAdjMines, adjacentSpots);
-    }
     return {
         isMine,
         amountOfAdjMines,
@@ -410,7 +407,7 @@ const performSpotReveal = function (x: number, y: number, callback?: SpotRevealC
     };
 };
 
-const revealSpot: (x: number, y: number) => boolean = function (x: number, y: number) {
+const revealSpot = function (x: number, y: number) {
     const queue = [{x, y}];
     const visited = new Set([y * gameState.gameOptions.boardWidth + x]);
     let head = 0;
@@ -442,19 +439,18 @@ const revealSpot: (x: number, y: number) => boolean = function (x: number, y: nu
                     }
                 }
             }
-            return true;
         }
     }
-
-    return visited.size > 0;
 };
 
 const revealMultiple: (spotArr: MineBlock[]) => boolean = function (spotArr: MineBlock[]) {
     let hasHitASpot = false;
     for (let i = 0; i < spotArr.length; i++) {
-        if (revealSpot(spotArr[i].x, spotArr[i].y)) {
-            hasHitASpot = true;
+        if (gameState.board[spotArr[i].y][spotArr[i].x].isRevealed) {
+            continue;
         }
+        revealSpot(spotArr[i].x, spotArr[i].y);
+        hasHitASpot = true;
     }
     return hasHitASpot;
 };